f23cef8af2292a1cb41f751c734a7060c15d51b0
moizmazhar37 <moeez.ch3737@gmail.com>
2023-11-03 12:03:25 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implement your login logic here
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button type="submit">Login</button>
+        </form>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
